f3a5c3295418b30409759341571e181ba08466df
Aymeric <48229088+JustAymeric@users.noreply.github.com>
2020-07-04 14:36:40 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('requirements.txt') as f:
     requirements = list(f.read().splitlines())
 
 setup(
-    name='ClassroomTools',
+    name='classroom_tools',
     packages=find_packages(),
     install_requires=requirements
 )
