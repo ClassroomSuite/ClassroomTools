e0197042f3871ceaf75e70fef8dd807562d78a1d
Aymeric <48229088+JustAymeric@users.noreply.github.com>
2020-07-16 17:00:53 -0400

    Update sync_with_template_repository.py

--- classroom_tools/student_repositories/sync_with_template_repository.py
+++ classroom_tools/student_repositories/sync_with_template_repository.py
@@ -11,13 +11,16 @@ parser.add_argument(
     help='GitHub personal access token with repo permissions'
 )
 parser.add_argument(
+    '--single_student_repository',
+    help='Student repo in format: "Organization/RepositoryName" (for a single student repository)'
+)
+parser.add_argument(
     '--org_name',
-    help='GitHub organization with student repositories'
+    help='GitHub organization with student repositories (for multiples student repositories)'
 )
 parser.add_argument(
     '--repo_filter',
-    required=True,
-    help='Prefix to filter repositories for as given assignment or exercise'
+    help='Prefix to filter repositories for as given assignment or exercise (for multiples student repositories)'
 )
 parser.add_argument(
     '--template_repo_fullname',
@@ -43,6 +46,14 @@ def get_files_from_repo(repo, path):
             yield content
 
 
+def get_repo(fullname, g: github.Github):
+    try:
+        return g.get_repo(full_name_or_id=fullname)
+    except Exception as e:
+        print(e)
+        raise Exception(f'Coun\'t get repo: {fullname}')
+
+
 def copy_file_to_repo(file, repo):
     try:
         old_file = repo.get_contents(path=file.path)
@@ -62,25 +73,37 @@ def copy_file_to_repo(file, repo):
         )
 
 
+def get_students_repositories(args, g: github.Github):
+    if args.org_name is not None:
+        try:
+            org = g.get_organization(login=args.org_name)
+            student_repos = list(
+                filter(
+                    lambda repo: args.repo_filter in repo.name,
+                    org.get_repos()
+                )
+            )
+            return student_repos
+        except Exception as e:
+            print(f'Coun\'t get organization: {args.org_name}')
+            print(e)
+            return []
+    elif args.single_student_repository is not None:
+        return [get_repo(args.single_student_repository, g)]
+
+
 if __name__ == '__main__':
     args = parser.parse_args()
     files_to_update = set(args.files_to_update)
     g = github.Github(login_or_token=args.token)
-    template_repo = g.get_repo(full_name_or_id=args.template_repo_fullname)
+    template_repo = get_repo(args.template_repo_fullname, g)
     template_files = list(
         filter(
             lambda file: file.path in files_to_update,
             get_files_from_repo(repo=template_repo, path='')
         )
     )
-    org = g.get_organization(login=args.org_name)
-    student_repos = list(
-        filter(
-            lambda repo: args.repo_filter in repo.name,
-            org.get_repos()
-        )
-    )
-    for repo in student_repos:
+    for repo in get_students_repositories(args, g):
         print(f'\nUpdating files in:\t{repo.full_name}\nwith files from:\t{template_repo.full_name}')
         for file in template_files:
             print(f'\tSyncing: {file.path}')
