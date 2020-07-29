d1879608fd238e47269c8872740e95524008f5a1
Aymeric <48229088+JustAymeric@users.noreply.github.com>
2020-07-29 03:43:40 -0400

    Create delete_workflow_runs.py

---
+++ classroom_tools/delete_workflow_runs.py
@@ -0,0 +1,47 @@
+import argparse
+
+import github
+import requests
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '--token',
+    required=True,
+    help='GitHub personal access token with admin permissions'
+)
+parser.add_argument(
+    '--repo_fullname',
+    required=True,
+    help='Repository fullname'
+)
+parser.add_argument(
+    '--delete_only_failed_runs',
+    default=False,
+    action='store_true',
+    help='Delete only workflow runs that failed'
+)
+
+
+def delete_workflow_run(workflow_url, token):
+    print(f'Deleting: {workflow_url}')
+    res = requests.delete(url=workflow_url,
+                          headers={'Authorization': f'token {token}'})
+    print(f'ok: {res.ok}')
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    g = github.Github(login_or_token=args.token)
+    repo = g.get_repo(full_name_or_id=args.repo_fullname)
+
+    if args.delete_only_failed_runs:
+        workflow_runs = list(
+            filter(
+                lambda run: run.conclusion == 'failure'
+            ),
+            repo.get_workflow_runs()
+        )
+    else:
+        workflow_runs = repo.get_workflow_runs()
+    for workflow in repo.get_workflow_runs():
+        delete_workflow_run(workflow.url, args.token)
