e42d9bdf65015d0d8f80806943002671893ddd35
Aymeric <48229088+JustAymeric@users.noreply.github.com>
2020-07-30 13:49:42 -0400

    Update sync_with_template_repository.py

--- classroom_tools/student_repositories/sync_with_template_repository.py
+++ classroom_tools/student_repositories/sync_with_template_repository.py
@@ -67,6 +67,10 @@ def get_files_to_update(files_to_update, template_repo):
 
 
 if __name__ == '__main__':
+    msg = 'Sync with template repository'
+    padding = 4 * '\n' + (50 - len(msg) // 2) * '#' + ' '
+    print(padding + msg + padding[::-1])
+    print('\n\n\n\n################################# Sync with template repository #################################\n')
     args = parser.parse_args()
     template_repo = github_utils.get_repo(args.template_repo_fullname, args.token)
     files_to_update = get_files_to_update(args.files_to_update, template_repo)
