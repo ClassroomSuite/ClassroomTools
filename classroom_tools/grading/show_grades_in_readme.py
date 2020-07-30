14d7d22dc990fb4a5f512aa92bdd546dc54c1565
Aymeric <48229088+JustAymeric@users.noreply.github.com>
2020-07-30 13:49:24 -0400

    Update show_grades_in_readme.py

--- classroom_tools/grading/show_grades_in_readme.py
+++ classroom_tools/grading/show_grades_in_readme.py
@@ -67,6 +67,9 @@ def update_readme(grades, readme_file):
 
 
 if __name__ == '__main__':
+    msg = 'Showing grades in README'
+    padding = 4 * '\n' + (50 - len(msg) // 2) * '#' + ' '
+    print(padding + msg + padding[::-1])
     dir_path = os.path.realpath(os.curdir)
     grades_file = os.path.join(dir_path, 'logs/grades.json')
     readme_file = os.path.join(dir_path, 'README.md')
