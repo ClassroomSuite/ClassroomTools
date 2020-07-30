9ea15c825c54a202b184f8e544419ed60bc54766
Aymeric <48229088+JustAymeric@users.noreply.github.com>
2020-07-30 14:10:31 -0400

    Update delete_file.py

--- classroom_tools/student_repositories/delete_file.py
+++ classroom_tools/student_repositories/delete_file.py
@@ -26,7 +26,7 @@ parser.add_argument(
 )
 
 if __name__ == '__main__':
-    print('Deleting file'.center(50, '='))
+    print('Deleting file'.center(80, '='))
     args = parser.parse_args()
     if args.token == '':
         raise EmptyToken(permissions='repo')
