348dba02a937e67173354279e4e6131d4723d32e
Aymeric <48229088+JustAymeric@users.noreply.github.com>
2020-07-31 18:48:18 -0400

    Create files_to_update.py

---
+++ classroom_tools/verifications/files_to_update.py
@@ -0,0 +1,40 @@
+import argparse
+
+import github
+from colorama import Fore, Style
+
+from classroom_tools import github_utils
+from classroom_tools.student_repositories.sync_with_template_repository import get_files_to_update
+
+parser = argparse.ArgumentParser('Verify that a repo has access to the secrets it needs')
+parser.add_argument(
+    '--token',
+    required=True,
+    help='GitHub personal access token with repo permissions'
+)
+parser.add_argument(
+    '--template_repo_fullname',
+    required=True,
+    help='Template repo used to create student repositories in format: OrgName/RepoName'
+)
+
+if __name__ == '__main__':
+    print('\n\n' + 'Verifying files_to_update.txt'.center(80, '='))
+    args = parser.parse_args()
+    print('Args:\n' + ''.join(f'\t{k}: {v}\n' for k, v in vars(args).items()))
+    g = github.Github(login_or_token=args.token)
+    template_repo = g.get_repo(full_name_or_id=args.template_repo_fullname)
+    files_to_update = get_files_to_update(files_to_update=None, template_repo=template_repo)
+    print(f'From repository: {template_repo.full_name}')
+    missing = []
+    for file in github_utils.get_files_from_repo(repo=template_repo, path=''):
+        if file.path in files_to_update:
+            print(f'{Fore.GREEN}\tFound: {file.path}')
+        else:
+            missing.append(file.path)
+    if len(missing) > 0:
+        for path in missing:
+            print(f'{Fore.RED}\tMissing: {path}')
+        exit(1)
+
+    print(Style.RESET_ALL)
