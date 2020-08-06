1c4e1368de4123fa13dfdbaac5317bf069fe1ec7
Aymeric <48229088+JustAymeric@users.noreply.github.com>
2020-08-06 01:09:53 -0400

    Create delete_workflows.py

---
+++ classroom_tools/student_repositories/delete_workflows.py
@@ -0,0 +1,45 @@
+import argparse
+
+from classroom_tools import github_utils
+
+parser = argparse.ArgumentParser('Delete all workflows from each selected student repositories (to prevent cheating)')
+parser.add_argument(
+    '--token',
+    required=True,
+    help='GitHub personal access token with repo and workflow permissions'
+)
+parser.add_argument(
+    '--org_name',
+    required=True,
+    help='GitHub organization name'
+)
+parser.add_argument(
+    '--repo_filter',
+    required=True,
+    help='Prefix to filter repositories for as given assignment or exercise'
+)
+
+
+def main(args):
+    print('\n\n' + 'Deleting workflows'.center(80, '='))
+    args = parser.parse_args(args)
+    print('Args:\n' + ''.join(f'\t{k}: {v}\n' for k, v in vars(args).items()))
+    github_utils.verify_token(args.token)
+    repositories = github_utils.get_students_repositories(
+        token=args.token,
+        org_name=args.org_name,
+        repo_filter=args.repo_filter
+    )
+    for repo in repositories:
+        print(f'Deleting workflows from repo: {repo.full_name}:')
+        deleted = github_utils.delete_all_workflows(repo)
+        for path in deleted:
+            print(f'\tRemoved: {path}')
+    print('\nSummary:')
+    print(f'\tTotal number of repositories updated: {len(repositories)}')
+
+
+if __name__ == '__main__':
+    import sys
+
+    main(sys.argv[1:])
