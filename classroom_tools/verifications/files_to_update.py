9b6e49aa8606343bb8b1f023c903d05a7c7cb1bc
Aymeric <48229088+JustAymeric@users.noreply.github.com>
2020-08-06 03:19:40 -0400

    Update files_to_update.py

--- classroom_tools/verifications/files_to_update.py
+++ classroom_tools/verifications/files_to_update.py
@@ -1,10 +1,9 @@
 import argparse
 
 import github
-from colorama import Fore, Style
+from colorama import Fore
 
 from classroom_tools import github_utils
-from classroom_tools.student_repositories.sync_with_template_repository import get_files_to_update
 
 parser = argparse.ArgumentParser('Validate paths specified in files_to_update.txt')
 parser.add_argument(
@@ -26,28 +25,23 @@ def main(args):
     github_utils.verify_token(args.token)
     g = github.Github(login_or_token=args.token)
     template_repo = g.get_repo(full_name_or_id=args.template_repo_fullname)
-    files_to_update = get_files_to_update(files_to_update=None, template_repo=template_repo)
-    print(f'From repository: {template_repo.full_name}')
-    missing = []
-    repo_files = set(
+    file = template_repo.get_contents('settings/files_to_update.txt')
+    paths_to_update = set(file.decoded_content.decode('utf-8').splitlines())
+    template_files = list(github_utils.get_files_from_repo(repo=template_repo, path=''))
+    template_paths = set(
         map(
             lambda file: file.path,
-            github_utils.get_files_from_repo(repo=template_repo, path='')
+            template_files
         )
     )
-    for path in files_to_update:
-        if path in repo_files:
-            print(f'{Fore.GREEN}\tFound: {path}')
-        else:
-            missing.append(path)
-    for path in repo_files.difference(files_to_update):
+    print(f'From repository: {template_repo.full_name}')
+    for path in paths_to_update.intersection(template_paths):
+        print(f'{Fore.GREEN}\tFound: {path}')
+    for path in template_paths.difference(paths_to_update):
         print(f'{Fore.YELLOW}\tNot included: {path}')
-    if len(missing) > 0:
-        for path in missing:
-            print(f'{Fore.RED}\tMissing: {path}')
-        exit(1)
-
-    print(Style.RESET_ALL)
+    for path in paths_to_update.difference(template_paths):
+        print(f'{Fore.RED}\tMissing: {path}')
+        raise Exception(f'{Fore.RED}Missing or incorrect paths')
 
 
 if __name__ == '__main__':
