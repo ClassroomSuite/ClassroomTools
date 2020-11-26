aa164305f633a217360c17a0bf59b466ea931fe8
Aymeric <48229088+JustAymeric@users.noreply.github.com>
2020-11-25 23:29:39 -0500

    Pretty json decode err msg for tests_associations_file

--- classroom_tools/grading/create_grades.py
+++ classroom_tools/grading/create_grades.py
@@ -4,37 +4,43 @@ import argparse
 import json
 import os
 
+from colorama import Fore
+
 parser = argparse.ArgumentParser()
 parser.add_argument(
-    '--test_associations_path',
-    required=True,
-    help='Path to test_associations.json'
+        '--test_associations_path',
+        required=True,
+        help='Path to test_associations.json'
 )
 
 
 def get_tests_results(log_file):
-    f = open(log_file)
-    results = []
-    for line in f.readlines():
-        test_fn_name, *_ = line.split(' ')
-        if ' ok' in line:
-            results.append({'func_name': test_fn_name, 'passing': True})
-        elif ' ERROR' in line or ' FAIL' in line:
-            results.append({'func_name': test_fn_name, 'passing': False})
-        else:
-            break
-    f.close()
+    with open(log_file) as f:
+        results = []
+        for line in f.readlines():
+            test_fn_name, *_ = line.split(' ')
+            if ' ok' in line:
+                results.append({'func_name': test_fn_name, 'passing': True})
+            elif ' ERROR' in line or ' FAIL' in line:
+                results.append({'func_name': test_fn_name, 'passing': False})
+            else:
+                break
     return results
 
 
 def add_tests_info(results, tests_associations_file):
-    f = open(tests_associations_file, encoding='UTF-8')
-    tests_associations = json.load(f)
-    for result in results:
-        test = tests_associations[result['func_name']]
-        result['test_name'] = test['name']
-        result['points'] = test['points']
-    f.close()
+    with open(tests_associations_file, encoding='UTF-8') as f:
+        try:
+            tests_associations = json.load(f)
+        except:
+            print(f'{Fore.RED}'
+                  f'Problem occurred while decoding tests_associations_file: {tests_associations_file}.'
+                  f'Verify JSON format of file.')
+            raise
+        for result in results:
+            test = tests_associations[result['func_name']]
+            result['test_name'] = test['name']
+            result['points'] = test['points']
 
 
 def main(args):
